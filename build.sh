1825a319d1a36efaf4ac4ce97e0739361c7f5a71
overader <aderjasmirzeasrocha@gmail.com>
2023-09-30 19:45:16 -0600

    agregar usuario

--- build.sh
+++ build.sh
@@ -2,8 +2,16 @@
 # exit on error
 set -o errexit
 
-#poetry install
+# Instalar dependencias
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Ejecutar migraciones
 python manage.py migrate
+
+# Recopilar archivos estáticos
+python manage.py collectstatic --no-input
+
+# Crear un superusuario automáticamente
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('overader', 'aderjasmirzeasrocha@gmail.com', '00040')" | python manage.py shell
+
+# Puedes reemplazar 'nombre_usuario', 'correo@ejemplo.com' y 'contraseña' con los valores deseados
